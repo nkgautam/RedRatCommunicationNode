a59ff6d34fb9dc51e2610fbdce380a776b2b3784
nkgautam <narendra.gautam1@gmail.com>
2019-09-03 14:47:38 +0530

    Web app alerts amd authentication services added

--- WebApp/src/app/app-routing.module.ts
+++ WebApp/src/app/app-routing.module.ts
@@ -1,24 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserAddComponent } from './user-add/user-add.component';  
-import { UserGetComponent } from './user-get/user-get.component';  
-import { UserEditComponent } from './user-edit/user-edit.component';  
+import { UserAddComponent } from './user-add/user-add.component';
+import { UserGetComponent } from './user-get/user-get.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-
-const routes: Routes = [  
-  {  
-    path: 'user/create',  
-    component: UserAddComponent  
-  },  
-  {  
-    path: 'edit/:id',  
-    component: UserEditComponent  
-  },  
-  {  
-    path: 'users',  
-    component: UserGetComponent  
-  }  
-];  
+const routes: Routes = [
+  {
+    path: 'user/create',
+    component: UserAddComponent
+  },
+  {
+    path: 'edit/:id',
+    component: UserEditComponent
+  },
+  {
+    path: 'users',
+    component: UserGetComponent
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  //{ path: '**', redirectTo: 'login' },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
